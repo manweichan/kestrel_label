7d4da112913c73c6c57db25f13156a56c6111f2f
MessierRank <50602771+MessierRank@users.noreply.github.com>
2019-05-21 11:41:08 -0400

    Update messierRank.js

--- messier/messierRank.js
+++ messier/messierRank.js
@@ -49,18 +49,6 @@ window.onkeydown = function(event) {
   }
 }
 
-function createArray(length) {
-    var arr = new Array(length || 0),
-        i = length;
-
-    if (arguments.length > 1) {
-        var args = Array.prototype.slice.call(arguments, 1);
-        while(i--) arr[length-1 - i] = createArray.apply(this, args);
-    }
-
-    return arr;
-}
-
 function logClick(winner, loser) {
   var ret;
 
